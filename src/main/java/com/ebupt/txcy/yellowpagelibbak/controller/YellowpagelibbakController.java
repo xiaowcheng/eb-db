cc36e7e7d4c66506553750dad75f87c4a81caf14
wangfeng_1 <wangfeng_1.@ebupt.com>
2019-12-09 10:58:08 +0800

    日志添加

--- src/main/java/com/ebupt/txcy/yellowpagelibbak/controller/YellowpagelibbakController.java
+++ src/main/java/com/ebupt/txcy/yellowpagelibbak/controller/YellowpagelibbakController.java
@@ -16,13 +16,14 @@ import com.ebupt.txcy.yellowpagelibbak.vo.Response;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@Slf4j
 @RestController
 @RequestMapping("yellowpagelibbak/v1/")
 @Api(value = "黄页备份库管理接口",tags = {"提供单个查询，分页查询，批量更新，批量添加，批量删除"})
@@ -35,11 +36,12 @@ public class YellowpagelibbakController {
 	@PostMapping("searchNumber")
 	public Response searchNumber(@RequestBody Map<String,String> input) {
 		String phoneCondition = input.get("phoneCondition");
+		log.info("接收到参数{}",phoneCondition);
 		if(CommonUtils.isBlank(phoneCondition)) {
 			return Response.error(Constants.PHOONENUMBER_REPSPONSE_PARAM);
 		}
 		Yellowpagelibbak yellowpagelibbak  = 	yellowpagelibbakService.searchNumber(phoneCondition);
-
+		log.info("单个数据查询完成：{}",yellowpagelibbak);
 		return Response.ok(yellowpagelibbak);
 	}
 	/**
@@ -54,6 +56,7 @@ public class YellowpagelibbakController {
 		String phoneCondition = input.get("phoneCondition");
 		String startStr = input.get("start");
 		String pageStr = input.get("page");
+		log.info("接收参数（phoneCondition,start,page）：{}，{}，{}",phoneCondition,startStr,pageStr);
 		Integer start = null;
 		Integer page =null;
 		if(CommonUtils.isNotBlank(startStr)) {
@@ -68,26 +71,32 @@ public class YellowpagelibbakController {
 			return Response.error(Constants.PHOONENUMBER_REPSPONSE_PARAM);
 		}
 		Pagination<Yellowpagelibbak> pagination = 	yellowpagelibbakService.searchNumberList(phoneCondition,start,page);
-		
+		log.info("数据处理完成");
 		return Response.ok(pagination.getList(),pagination.getCount());
 	}
 	@PostMapping("addNumber")
 	public Response addNumber(@RequestBody YellowpagelibbakRequestBody yellowpagelibbakRequestBody) {
+		log.info("添加数据");
 		List<Yellowpagelibbak> yellowpagelibbaks = yellowpagelibbakRequestBody.getPhoneList();
 		List<PhoneListResponse> phoneListResponses =  yellowpagelibbakService.addNumbers(yellowpagelibbaks);
+		log.info("数据处理完成");
 		return Response.ok(phoneListResponses);
 	}
 	@PostMapping("updateNumber")
 	public Response updateNumber(@RequestBody YellowpagelibbakRequestBody yellowpagelibbakRequestBody) {
+		log.info("更新数据");
 		List<Yellowpagelibbak> yellowpagelibbaks = yellowpagelibbakRequestBody.getPhoneList();
 		List<PhoneListResponse> phoneListResponses = yellowpagelibbakService.updateNumber(yellowpagelibbaks);
+		log.info("数据处理完成");
 		return Response.ok(phoneListResponses);
 	}
 	
 	@PostMapping("delNumber")
 	public Response delNumber(@RequestBody YellowpagelibbakRequestBody yellowpagelibbakRequestBody) {
+		log.info("删除号码");
 		List<Yellowpagelibbak> yellowpagelibbaks = yellowpagelibbakRequestBody.getPhoneList();
 		List<PhoneListResponse> phoneListResponses = yellowpagelibbakService.delNumber(yellowpagelibbaks);
+		log.info("数据处理完成");
 		return Response.ok(phoneListResponses);
 	}
 	
