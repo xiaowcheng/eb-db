fa12dfc886c180c7893416cc48eb24df78805493
wangfeng_1 <wangfeng_1.@ebupt.com>
2020-02-25 17:41:08 +0800

    wf:添加部分日志

--- src/test/java/com/ebupt/txcy/microservicewhitelistserver/MicroserviceWhitelistserverApplicationTests.java
+++ src/test/java/com/ebupt/txcy/microservicewhitelistserver/MicroserviceWhitelistserverApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ebupt.txcy.policewhite;
+package com.ebupt.txcy.microservicewhitelistserver;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes ={} )
 public class MicroserviceWhitelistserverApplicationTests {
 
     @Test
